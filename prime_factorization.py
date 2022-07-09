306c4f4f259108eba3e6bac0797949df038c615a
kanekop <36024074+kanekop@users.noreply.github.com>
2022-07-10 08:26:01 +0900

    Update prime_factorization.py

--- prime_factorization.py
+++ prime_factorization.py
@@ -1,9 +1,3 @@
-#PE5
-
-"""
-2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-"""
 def eratosthenes(primes, N):
     for i in range(2, N+1):
         primes[i] = True
