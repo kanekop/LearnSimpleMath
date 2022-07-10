7a50a2768658eae4c1b3de563c9c967222f736df
kanekop <36024074+kanekop@users.noreply.github.com>
2022-07-10 13:13:27 +0900

    Update sieve_of_eratosthenes.py

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -1,4 +1,3 @@
-#Sieve of Eratosthenes
 def eratosthenes(primes, N):
     for i in range(2, N+1):
         primes[i] = True
@@ -6,6 +5,7 @@ def eratosthenes(primes, N):
 
     #sieving
     # 2 = true
+    #sqrt
     for i in range(2, N+1):
         if primes[i] == True and i**2 <= N:
             for j in range(N+1):
@@ -20,6 +20,37 @@ def eratosthenes(primes, N):
             primes.pop(i)
     return list(primes.keys())
 
+def factorization(aP, N, f):
+    if N == 1:
+        return True
+    if N in aP:
+        f.append(N)
+        factorization(aP, 1, f)
+        return True
+    else:
+        q = N
+        for i in range(len(aP)):
+            if q%aP[i] == 0:
+                f.append(aP[i])
+                q = q//aP[i]
+                if q > 1:
+                    factorization(aP, q, f)
+                    return True
+                else:
+                    return True
+
+    return True
+
+N = 2
 a = {}
-aP = []
 aP = eratosthenes (a, N)
+while len(aP) < 10001:
+    a = {}
+    f = []
+    aP = eratosthenes (a, N)
+    N *= 2
+    
+
+print(aP[10000])
+#104743
+
